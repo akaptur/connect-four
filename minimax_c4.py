ee840c0ff71f0bac0aec013af0d0604b246ca350
Allison Kaptur <allison.kaptur@gmail.com>
2013-04-09 13:47:26 -0400

    More general and more compact row victory checker

--- minimax_c4.py
+++ minimax_c4.py
@@ -1,4 +1,4 @@
-import connect_four
+# from connect_four import get_row
 import pprint
 
 ROWS = 3
@@ -10,22 +10,28 @@ MIN = 2
 
 BLANK_BOARD = [[0 for i in range(COLUMNS)] for j in range(ROWS)]
 
+def get_row(grid, col, rows): # copied this in here so I don't have to deal with pygame
+    for i, row in enumerate(reversed(grid)):
+    #if it's over a column, figure out the bottom
+    #slot in the grid to put the piece
+        if row[col] == 0:
+            return rows-i-1
+    return None
+
 def is_row_win(grid):
-    mr = (COLUMNS + 1)/2
     for row in reversed(grid):
-        streak = 0
-        if row[mr] != 0:
-            is_winner = row[mr]
-            for column in row:
-                if column == is_winner:
-                    streak += 1
-                else:
-                    streak = 0
-                if streak >= TO_WIN:
-                    return is_winner
+        for start in range(COLUMNS - TO_WIN + 1):
+            connected_squares = [row[start+i] for i in range(TO_WIN)]
+            print connected_squares
+            if len(set(connected_squares)) == 1:
+                member = connected_squares.pop() # get the only member of the set
+                    if member != 0:
+                        return member
     return None
 
 def is_col_win(grid):
+    for col in range(COLUMNS):
+        pass
     mc = grid[((ROWS+1)/2)]
     for i in range(COLUMNS):
         #check to see if row3 and 4 have a match first
@@ -82,21 +88,21 @@ def determine_winner(grid):
 
 def make_sample_board(b):
     r = ROWS
-    b[connect_four.get_row(b,1,ROWS)][1] = MAX
-    b[connect_four.get_row(b,0,ROWS)][0] = MIN
-    b[connect_four.get_row(b,1,ROWS)][1] = MAX
-    b[connect_four.get_row(b,1,ROWS)][1] = MIN
-    b[connect_four.get_row(b,0,ROWS)][0] = MAX
-    b[connect_four.get_row(b,2,ROWS)][2] = MIN
+    b[get_row(b,1,ROWS)][1] = MAX
+    b[get_row(b,0,ROWS)][0] = MIN
+    b[get_row(b,1,ROWS)][1] = MAX
+    b[get_row(b,1,ROWS)][0] = MIN
+    b[get_row(b,0,ROWS)][1] = MAX
+    b[get_row(b,2,ROWS)][2] = MIN
     return b
 
 def make_move(b, player):
     boards = []
     for i in range(COLUMNS):
         new_b = [row[:] for row in b]
-        row = connect_four.get_row(b, i, ROWS)
+        row = get_row(b, i, ROWS)
         if row != None:
-            new_b[connect_four.get_row(b, i, ROWS)][i] = player
+            new_b[get_row(b, i, ROWS)][i] = player
             boards.append([new_b[:],i] )
     return boards
 
@@ -113,7 +119,7 @@ def get_max_or_min(possible_moves, player):
 def recur_add_player_depth(board, player, boards=[], col=0):
     pp.pprint(board)
     import pdb
-    pdb.set_trace()
+    # pdb.set_trace()
     winner = determine_winner(board)
     if winner:
         if winner == MAX:
@@ -161,19 +167,18 @@ diag_win_EXAMPLE = [[0, 0, 0], [0, 0, 0], [1, 2, 0], [2, 1, 0], [2, 1, 1]]
 
 
 if __name__ == '__main__':
-    board = make_sample_board(BLANK_BOARD)
-    pp = pprint.PrettyPrinter(width = 20)
+    # board = make_sample_board(BLANK_BOARD)
+    # pp = pprint.PrettyPrinter(width = 20)
 
-    assert no_more_moves(board) == False
-    assert no_more_moves(no_moves_EXAMPLE) == True
+    # assert no_more_moves(board) == False
+    # assert no_more_moves(no_moves_EXAMPLE) == True
 
-    print recur_add_player_depth(board, MAX)
-    '''
-    first_level = play_board(board, 1)
-    print_board(board, 5)
+    # print recur_add_player_depth(board, MAX)
+    # first_level = play_board(board, 1)
+    # print_board(board, 5)
 
-    assert first_level == min_1_SOLUTION
+    # assert first_level == min_1_SOLUTION
     assert is_row_win(row_win_EXAMPLE) == True
+    pprint.pprint(col_win_EXAMPLE)
     assert is_col_win(col_win_EXAMPLE) == True
-    assert is_diag_win(diag_win_EXAMPLE) == True
-    '''
+    # assert is_diag_win(diag_win_EXAMPLE) == True
